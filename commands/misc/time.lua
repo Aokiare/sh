8f22851004acdaab8a260054eb5c3d6628e04d26
slowstab <36301891+slowstab@users.noreply.github.com>
2021-05-23 14:38:35 +0200

    *bonk* retard
    resolve conflicts resulted from cherry picking commits in a separate commit like a genius

--- commands/misc/time.lua
+++ commands/misc/time.lua
@@ -4,9 +4,9 @@ return {
     hidden = false,
     command = function (message)
         local author = message.guild:getMember(client.owner.id)
-        local time = os.date("%I:%M:%S %p", os.time())
-        local date = os.date("%d %B %Y", os.time())
-        local dayoftheweek = os.date("%A", os.time())
+        local time = os.date("%I:%M:%S %p", os.time() + 2 * 60 * 60)
+        local date = os.date("%d %B %Y", os.time() + 2 * 60 * 60)
+        local dayoftheweek = os.date("%A", os.time() + 2 * 60 * 60)
         message:reply({embed = {author = {name = author.tag, icon_url = author:getAvatarURL()}, fields = {{name = "Time", value = time}, {name = "Date", value = date}, {name = "Day", value = dayoftheweek:upper()}}, thumbnail = {url = "https://i.imgur.com/9Tq1txG.png"},color = botColor;}})
     end
 }
--- modules/config.lua
+++ modules/config.lua
@@ -1,2 +1,4 @@
 _G.prefix = ">"
 _G.botToken = os.getenv("botToken")
+_G.botColor = 0xa57562
+_G.successColor = 0x43B581
