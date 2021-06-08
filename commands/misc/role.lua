06c29e5152bab9430ca895c3412a5a7df72c5939
slowstab <36301891+slowstab@users.noreply.github.com>
2021-06-09 00:19:39 +0200

    lowercase fag >w<

--- commands/misc/role.lua
+++ commands/misc/role.lua
@@ -13,7 +13,7 @@ return {
             message:reply(err)
         else
             message:addReaction("✨")
-            message:reply({ embed = {fields = {{name = "name", value = role.name, inline = true}, {name = "color", value = discordia.Color(role.color):toHex(), inline = true}, {name = "members", value = role.members:count(), inline = true}, {name = "mention", value = role.mentionString, inline = true}, {name = "hoisted", value = role.hoisted, inline = true}, {name = "mentionable", value = role.mentionable, inline = true}, {name = "position", value = role.position, inline = true}, {name = "perms", value = utils.tableToString(role:getPermissions():toArray()), inline = false}}, color = discordia.Color(role.color).value, footer = {text = "ID: "..role.id.." • Today at "..os.date("%I:%M %p", os.time())}}})
+            message:reply({ embed = {fields = {{name = "name", value = role.name, inline = true}, {name = "color", value = tostring(discordia.Color(role.color):toHex()):lower(), inline = true}, {name = "members", value = role.members:count(), inline = true}, {name = "mention", value = role.mentionString, inline = true}, {name = "hoisted", value = tostring(role.hoisted):lower(), inline = true}, {name = "position", value = role.position, inline = true}, {name = "mentionable", value = tostring(role.mentionable):lower(), inline = true}, {name = "perms", value = utils.tableToString(role:getPermissions():toArray()), inline = false}}, color = discordia.Color(role.color).value, footer = {text = "ID: "..role.id.." • Today at "..os.date("%I:%M %p", os.time())}}})
         end
     end
 }
