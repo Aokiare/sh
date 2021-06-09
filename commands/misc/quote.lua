658c9ada3a1449b1709ba673df440b4c8a9465e4
slowstab <36301891+slowstab@users.noreply.github.com>
2021-06-09 13:55:09 +0200

    improve (?) quote command
    adds support for quoting messages from other channels but delays quoting args if server has a lot of channels ¯\_(ツ)_/¯

--- commands/misc/quote.lua
+++ commands/misc/quote.lua
@@ -4,19 +4,33 @@ return {
     hidden = false,
     command = function (message)
         local msg, channel, member, content
+
         if message.channel:getMessage(args) then
             msg = message.channel:getMessage(args)
             content = msg.content
-        elseif args then
-            msg = message
-            message:delete()
-            content = msg.content:gsub("%"..prefix.."quote ","")
+            channel = msg.guild:getChannel(msg.channel.id)
         else
-            message:reply(err)
-            return
+            for txtchnl in message.guild.textChannels:iter() do
+                if txtchnl:getMessage(args) then
+                    msg = txtchnl:getMessage(args)
+                    content = msg.content
+                    channel = txtchnl
+                break
+                end
+            end
+            if not msg then
+                if args then
+                    msg = message
+                    message:delete()
+                    content = msg.content:gsub("%"..prefix.."quote ","")
+                    channel = msg.guild:getChannel(msg.channel.id)
+                else
+                    message:reply(err)
+                return
+                end
+            end
         end
         member = msg.member
-        channel = msg.guild:getChannel(msg.channel.id)
         message:reply({embed={author = {name = member.tag, icon_url = member:getAvatarURL(1024)}, color = member:getColor().value, description = content, footer = {text = "#"..channel.name.." in "..msg.guild.name.." • "..os.date("%d/%m/%Y at %I:%M:%S%p", msg.createdAt + 2 * 60 * 60)}}})
     end
 }
