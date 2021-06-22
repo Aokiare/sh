b0d40b69f992753105f1a2195ddf63be4845a916
slowstab <36301891+slowstab@users.noreply.github.com>
2021-06-22 21:38:04 +0200

    add a bunch of voice commands

---
+++ commands/admin/deafen.lua
@@ -0,0 +1,29 @@
+return {
+	name = "deafen",
+    description = "deafen someone in vc",
+    hidden = false,
+    command = function (message)
+        local author = message.guild:getMember(message.author.id)
+        if not author:hasPermission("deafenMembers") or not author.id == owner.id then
+            message:reply("nice try retard")
+        else
+            local member
+            if message.mentionedUsers.first then
+                member = message.guild:getMember(message.mentionedUsers.first.id)
+            elseif message.guild:getMember(args) then
+                member = message.guild:getMember(args)
+            else
+                member = author
+            end
+
+            if not member or not member.voiceChannel then
+                message:reply(err)
+            else
+                message:addReaction("✨")
+                member:deafen()
+                message:reply({embed = {color = successColor, description = "<:shSuccess:835619376052174848> deafened **"..member.tag.."**"}})
+            return
+            end
+        end
+    end
+}
