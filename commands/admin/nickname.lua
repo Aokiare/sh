cb9732e83a90f6d907aa504d1679bea796174fe7
slowstab <36301891+slowstab@users.noreply.github.com>
2021-06-26 18:42:43 +0200

    fix nickname cmd

--- commands/admin/nickname.lua
+++ commands/admin/nickname.lua
@@ -5,50 +5,51 @@ return {
     command = function (message)
         local author = message.guild:getMember(message.author.id)
         local bot = message.guild:getMember(client.user.id)
-        if author:hasPermission("manageNicknames") or author.id == client.owner.id then
+        if not author:hasPermission("manageNicknames") or not author.id == client.owner.id then return message:reply(err) end
             local argsTable = string.split(args," ")
-            local member
+            local member, nickname
             if message.mentionedUsers.first then
                 member = message.guild:getMember(message.mentionedUsers.first.id)
+                nickname = argsTable[2]
             elseif message.guild:getMember(argsTable[1]) then
                 member = message.guild:getMember(argsTable[1])
-            end
-            if not member then
-                message:reply(err)
-            return end
-            if author.highestRole.position < member.highestRole.position then
-                message:reply("discord says ur a peasent compared to "..member.mentionString.." so no fuk u lol")
-            return
+                nickname = argsTable[2]
             else
-                if bot.highestRole.position < member.highestRole.position then
-                    message:reply({
-                        embed = {
-                            description ="<:shError:835619357249241159> **"..member.mentionString.."** has a higher role than me so i cant do that",
-                            color = discordia.Color.fromHex("#EA4445").value
-                        }
-                    })
-                return
-                else
-                    message:addReaction("✨")
-                    if argsTable[2] then
-                        member:setNickname(argsTable[2])
-                        message:reply({
-                            embed = {
-                                color = successColor,
-                                description = "<:shSuccess:835619376052174848> changed **"..member.mentionString.."** nickname to **"..argsTable[2].."**"
-                            }
-                        })
-                    else
-                        member:setNickname()
-                        message:reply({
-                            embed = {
-                                color = successColor,
-                                description = "<:shSuccess:835619376052174848> cleared **"..member.mentionString.."** nickname"
-                            }
-                        })
-                    end
-                end
+                member = message.guild:getMember(message.author.id)
+                nickname = argsTable[1]
             end
-        end
+            if not member then return message:reply(err) end
+            if author.highestRole.position < member.highestRole.position then return message:reply({
+                    embed = {
+                        description ="<:shError:835619357249241159> **"..member.mentionString.."** has a higher role than you",
+                        color = discordia.Color.fromHex("#EA4445").value
+                    }
+                })
+            end
+            if bot.highestRole.position < member.highestRole.position then return message:reply({
+                    embed = {
+                        description ="<:shError:835619357249241159> **"..member.mentionString.."** has a higher role than me so i cant do that",
+                        color = discordia.Color.fromHex("#EA4445").value
+                    }
+                })
+            end
+            message:addReaction("✨")
+            if nickname then
+                member:setNickname(nickname)
+                message:reply({
+                    embed = {
+                        color = successColor,
+                        description = "<:shSuccess:835619376052174848> changed **"..member.mentionString.."** nickname to **"..nickname.."**"
+                    }
+                })
+            else
+                member:setNickname()
+                message:reply({
+                    embed = {
+                        color = successColor,
+                        description = "<:shSuccess:835619376052174848> cleared **"..member.mentionString.."** nickname"
+                    }
+                })
+            end
     end
 }
