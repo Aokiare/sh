0b38d94aa3c8240b9ab70b386a74262dd25a9dcd
slowstab <36301891+slowstab@users.noreply.github.com>
2021-06-27 23:58:34 +0200

    add webm command

--- commands/misc/webm.lua
+++ commands/misc/webm.lua
@@ -1,16 +1,11 @@
 return {
-    name = "cute",
-    description = "get an image from a cute board",
+    name = "webm",
+    description = "get a random webm from a /wsg/",
     hidden = false,
     command = function (message)
         math.randomseed(os.time())
 
-        local function getBoard()
-            local boards = {"c", "cm"}
-            return boards[math.random(1,2)]
-        end
-
-        local targetBoard = getBoard()
+        local targetBoard = "wsg"
 
         local function getThread()
             local _res, data = http.request("GET", "https://a.4cdn.org/"..targetBoard.."/catalog.json")
@@ -18,39 +13,37 @@ return {
             return tonumber(parsedData[math.random(1, 10)]["threads"][math.random(1, 15)]["no"])
         end
 
-        local function getReplyImage(threadNumber)
+        local function getReplyWebm(threadNumber)
             local _res, data = http.request("GET", "https://a.4cdn.org/"..targetBoard.."/thread/"..threadNumber..".json")
             local parsedData = json.decode(data)
             local postNumber = math.random(1, #parsedData["posts"])
             return {
-            tonumber(parsedData["posts"][postNumber]["tim"]), -- image name
-            parsedData["posts"][postNumber]["ext"], -- image extension
+            tonumber(parsedData["posts"][postNumber]["tim"]), -- webm name
+            parsedData["posts"][postNumber]["ext"], -- extension
             parsedData["posts"][postNumber]["no"], -- post number
             parsedData["posts"][postNumber]["time"], -- post time
-            parsedData["posts"][postNumber]["filename"] -- image filename
+            parsedData["posts"][postNumber]["filename"] -- webm filename
             }
         end
 
-        local wallpaperData = getReplyImage(getThread())
+        local webmData = getReplyWebm(getThread())
 
-        while not wallpaperData[1] do
-            wallpaperData = getReplyImage(getThread())
+        while not webmData[1] or webmData[2] ~= ".webm" do
+            webmData = getReplyWebm(getThread())
         end
 
+        message:reply("https://i.4cdn.org/"..targetBoard.."/"..webmData[1]..webmData[2])
         message:reply({
             embed = {
                 color = botColor,
-                image = {
-                    url = "https://i.4cdn.org/"..targetBoard.."/"..wallpaperData[1]..wallpaperData[2]
-                },
-                title = "No."..wallpaperData[3],
-                description = wallpaperData[5]..wallpaperData[2],
+                title = "No."..webmData[3],
+                description = webmData[5]..webmData[2],
                 author = {
                     name = "/"..targetBoard.."/",
                     icon_url = "https://i.imgur.com/XcCKhYj.png"
                 },
                 footer = {
-                    text = wallpaperData[1].." • "..os.date("%d %b %Y %I:%M:%S %p" ,wallpaperData[4])
+                    text = webmData[1].." • "..os.date("%d %b %Y %I:%M:%S %p" ,webmData[4])
                 }
             }
         })
