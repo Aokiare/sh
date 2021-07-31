18ead1e5d1f548f9c9fa31e85a78a738e3218cfc
obscurity <obscurity@c.x4.pm>
2021-07-31 13:26:28 +0300

    vps shit

--- modules/config.lua
+++ modules/config.lua
@@ -1,6 +1,6 @@
 -- Licensed under the Open Software License version 3.0
 
-_G.prefix = ">>"
+_G.prefix = ">"
 _G.botToken = assert(FileReader.readFileSync("./token"))
 _G.botColor = 0xa57562
 _G.successColor = 0x43B581
