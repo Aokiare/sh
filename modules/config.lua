c1781ad9f95df5aafb62edf54d565dc5bb370de2
obscurity <z@x4.pm>
2021-08-23 20:26:37 +0200

    add domain command

--- modules/config.lua
+++ modules/config.lua
@@ -2,5 +2,9 @@
 
 _G.prefix = ">"
 _G.botToken = os.getenv("botToken")
+_G.njallaToken = assert(FileReader.readFileSync("./njalla"))
 _G.botColor = 0xa57562
 _G.successColor = 0x43B581
+_G.successEmote = "<:shSuccess:835619376052174848>"
+_G.failColor = 0xEA4445
+_G.failEmote = "<:shError:835619357249241159>"
