88fa65e53c04d7bcbfc36348dbe054682f0a9c73
obscurity <z@x4.pm>
2021-08-24 12:59:45 +0200

    small cleanup, too lazy to do a full one

--- modules/config.lua
+++ modules/config.lua
@@ -4,6 +4,7 @@ _G.prefix = ">"
 _G.botToken = os.getenv("botToken")
 _G.njallaToken = assert(FileReader.readFileSync("./njalla"))
 _G.botColor = 0xa57562
+_G.botEmote = "<a:rosebox_hearts:842568705015021590>"
 _G.successColor = 0x43B581
 _G.successEmote = "<:shSuccess:835619376052174848>"
 _G.failColor = 0xEA4445
