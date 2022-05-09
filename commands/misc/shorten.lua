3a649217409659ba9f277d2e65b81fa26b8d0033
obscurity <z@x4.pm>
2022-05-09 06:01:28 +0200

    fix(shorten): use `envs.sh`

--- commands/misc/shorten.lua
+++ commands/misc/shorten.lua
@@ -4,23 +4,23 @@ return {
     name = "shorten",
     description = "shorten a link",
     hidden = false,
-    command = function (message)
+    command = function(message)
         if not args then return message:reply(err) end
         local headers = {
-            {"Content-Type", "application/x-www-form-urlencoded"},
-            {"Accept", "application/json"}
+            { "Content-Type", "application/x-www-form-urlencoded" },
+            { "Accept", "application/json" }
         }
-        local data = "shorten="..args
-        local response, result = http.request("POST", "https://0x0.st", headers, data)
+        local data = "shorten=" .. args
+        local response, result = http.request("POST", "https://envs.sh", headers, data)
         if response.code ~= 200 then
             message:reply({ embed = {
                 color = failColor,
-                description = failEmote.." http request failed with code **"..response.code.."**"
-            }})
+                description = failEmote .. " http request failed with code **" .. response.code .. "**"
+            } })
         else
             message:reply({
                 embed = {
-                    description = botEmote.." "..result,
+                    description = botEmote .. " " .. result,
                     color = botColor
                 }
             })
